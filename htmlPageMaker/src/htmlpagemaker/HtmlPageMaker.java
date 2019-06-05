dcdf0849c6211268c59392933edeea4ddb1092cb
ElhusseinKhaled <elhussein2222003@gmail.com>
2019-06-05 07:24:46 +0200

    Add files via upload

---
+++ htmlPageMaker/src/htmlpagemaker/HtmlPageMaker.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htmlpagemaker;
+
+/**
+ *
+ * @author husseinegypt
+ */
+public class HtmlPageMaker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        htmlPageM h1;
+        h1 = new htmlPageM();
+        
+        h1.setVisible(true);
+        h1.setLocationRelativeTo(null);
+        
+    }
+    
+}
